7c287d5a6869f5881d036adb6b4b2ea282df4507
rydlande <rydlande@hotmail.com>
2023-05-14 18:46:25 +0200

    finished the contact page

--- js/contact.js
+++ js/contact.js
@@ -25,3 +25,49 @@ menuItems.forEach(
   }
 )
 // NAVBAR //
+
+
+// FORM //
+const form = document.querySelector("form#contactForm");
+form.addEventListener("submit", validateForm);
+
+const fullName = document.querySelector("input#contactName");
+const subject = document.querySelector("input#contactSubject");
+const email = document.querySelector("input#contactEmail");
+const content = document.getElementById("formContent").value;       //siden det er textarea, ikke input
+
+const nameMessage = document.querySelector("#nameMessage");
+const subjectMessage = document.querySelector("#subjectMessage");
+const emailMessage = document.querySelector("#emailMessage");
+const contentMessage = document.querySelector("#contentMessage");
+
+
+function validateForm(e) {
+  e.preventDefault();
+
+  let subName = fullName.value.trim();
+      nameMessage.innerHTML = "";
+      if (subName.length == 0) {
+        nameMessage.innerHTML = "Requried";
+      }
+    
+  let subSubject = subject.value.trim();
+      subjectMessage.innerHTML = "";
+      if (subSubject.length < 15) {
+        subjectMessage.innerHTML = "Subject must be at least 15 characters long";
+      }
+
+  let subEmail = email.value.trim();
+      emailMessage.innerHTML = "";
+      let emailPattern = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
+      if (!emailPattern.test(subEmail)) {
+        emailMessage.innerHTML = "Please enter a valid email";
+      }
+
+  let subContent = content.trim();
+      contentMessage.innerHTML = "";
+      if (subContent.length < 25) {
+        contentMessage.innerHTML = "Please tell us a bit more (at least 25 characters)";
+      }
+}
+// FORM //
