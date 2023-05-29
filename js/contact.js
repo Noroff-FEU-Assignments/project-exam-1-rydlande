90a1761b104ef400ecc8ca7e0ac20f8e656c6242
rydlande <rydlande@hotmail.com>
2023-05-29 21:35:11 +0200

    fikset en del av form som ikke fungerte

--- js/contact.js
+++ js/contact.js
@@ -34,7 +34,7 @@ form.addEventListener("submit", validateForm);
 const fullName = document.querySelector("input#contactName");
 const subject = document.querySelector("input#contactSubject");
 const email = document.querySelector("input#contactEmail");
-const content = document.getElementById("formContent").value;       //siden det er textarea, ikke input
+const content = document.getElementById("formContent");
 
 const nameMessage = document.querySelector("#nameMessage");
 const subjectMessage = document.querySelector("#subjectMessage");
@@ -46,28 +46,38 @@ function validateForm(e) {
   e.preventDefault();
 
   let subName = fullName.value.trim();
-      nameMessage.innerHTML = "";
-      if (subName.length == 0) {
-        nameMessage.innerHTML = "Requried";
-      }
-    
+  nameMessage.innerHTML = "";
+  if (subName.length == 0) {
+    nameMessage.innerHTML = "Required";
+  }
+
   let subSubject = subject.value.trim();
-      subjectMessage.innerHTML = "";
-      if (subSubject.length < 15) {
-        subjectMessage.innerHTML = "Subject must be at least 15 characters long";
-      }
+  subjectMessage.innerHTML = "";
+  if (subSubject.length < 15) {
+    subjectMessage.innerHTML = "Subject must be at least 15 characters long";
+  }
 
   let subEmail = email.value.trim();
-      emailMessage.innerHTML = "";
-      let emailPattern = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
-      if (!emailPattern.test(subEmail)) {
-        emailMessage.innerHTML = "Please enter a valid email";
-      }
+  emailMessage.innerHTML = "";
+  let emailPattern = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
+  if (!emailPattern.test(subEmail)) {
+    emailMessage.innerHTML = "Please enter a valid email";
+  }
 
-  let subContent = content.trim();
-      contentMessage.innerHTML = "";
-      if (subContent.length < 25) {
-        contentMessage.innerHTML = "Please tell us a bit more (at least 25 characters)";
-      }
+  let subContent = content.value.trim();
+  contentMessage.innerHTML = "";
+  if (subContent.length < 25) {
+    contentMessage.innerHTML = "Please tell us a bit more (at least 25 characters)";
+  }
+
+  if (
+    subName.length > 0 &&
+    subSubject.length >= 15 &&
+    emailPattern.test(subEmail) &&
+    subContent.length >= 25
+  ) {
+    successMessage.innerHTML = "Form submitted successfully!";
+    form.reset();
+  }
 }
 // FORM //
